76890603724b833580e75e13b8ef2d4ba559aa42
Debraj <debrajdasbdn@gmail.com>
2022-11-08 16:08:42 +0530

    url

--- src/Components/bootApi.js
+++ src/Components/bootApi.js
@@ -1,2 +1,2 @@
-const base_url="http://ec2-52-196-62-162.ap-northeast-1.compute.amazonaws.com:8083/api/v1.0/tweets";
+const base_url="http://ec2-18-183-161-117.ap-northeast-1.compute.amazonaws.com:8083/api/v1.0/tweets";
 export default base_url;
